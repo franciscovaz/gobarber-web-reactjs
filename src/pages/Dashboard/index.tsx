3497aa90d262aa88a7df6f0c75bf4cb5d0f41280
Francisco Vaz <ftomasvaz@gmail.com>
2020-05-24 09:20:48 +0100

    dashboard - next appointment

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiClock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
@@ -28,6 +37,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars2.githubusercontent.com/u/27808014?s=460&u=cf949e2367d64368dee7273a81e2ff061956052e&v=4"
+                alt="Francisco Vaz"
+              />
+
+              <strong>Francisco Vaz</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -58,3 +58,102 @@ export const Profile = styled.div`
     color: #ff9000;
   }
 `;
+
+export const Content = styled.main`
+  max-width: 1120px;
+  margin: 64px auto;
+  display: flex;
+`;
+
+export const Schedule = styled.div`
+  flex: 1;
+  margin-right: 120px;
+
+  h1 {
+    font-size: 36px;
+  }
+
+  p {
+    margin-top: 8px;
+    color: #ff9000;
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+  }
+
+  span {
+    display: flex;
+    align-items: center;
+  }
+
+  span + span::before {
+    content: '';
+    width: 1px;
+    height: 12px;
+    background: #ff9000;
+    margin: 0 8px;
+  }
+  /* span + span {
+    margin-left: 8px;
+    padding-left: 8px;
+    border-left: 1px solid #ff9000;
+  } */
+`;
+
+export const NextAppointment = styled.div`
+  margin-top: 64px;
+
+  > strong {
+    color: #999591;
+    font-size: 20px;
+    font-weight: 400;
+  }
+
+  div {
+    background: #3e3b47;
+    display: flex;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 10px;
+    margin-top: 24px;
+    /* relative para colocar a border no :before */
+    position: relative;
+
+    &::before {
+      position: absolute;
+      width: 1px;
+      height: 80%;
+      left: 0;
+      top: 10%;
+      background: #ff9000;
+      content: '';
+    }
+
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+    }
+
+    strong {
+      margin-left: 24px;
+      color: #fff;
+    }
+
+    span {
+      margin-left: auto;
+      display: flex;
+      align-items: center;
+      color: #999591;
+    }
+
+    svg {
+      color: #ff9000;
+      margin-right: 8px;
+    }
+  }
+`;
+
+export const Calendar = styled.aside`
+  width: 380px;
+`;
