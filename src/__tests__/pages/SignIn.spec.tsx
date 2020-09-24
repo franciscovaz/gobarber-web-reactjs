8c7d2091f3073b38b73e6938db131b0f598b9906
Francisco Vaz <ftomasvaz@gmail.com>
2020-09-24 22:56:34 +0100

    passing error of mock use history expect navigate to dashboard

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
 import SignIn from '../../pages/SignIn';
 
+// Crio a funcao push fora para os testes terem acesso e verem se foi disparada
+const mockedHistoryPush = jest.fn();
+
 // Todos os testes depois do mock ficam afetados por ele
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(), // quando aparecer o useHistory eu chamo uma funcao vazia
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }), // quando aparecer o useHistory eu chamo uma funcao push
     Link: ({ children }: { children: React.ReactNode }) => children, // Tambem uso os Link que mostram o conteudo dos children (Ver Link em SignIn)
   };
 });
@@ -22,5 +27,7 @@ describe('SignIn Page', () => {
     fireEvent.change(passwordField, { target: { value: '123456' } });
 
     fireEvent.click(buttonElement);
+
+    expect(buttonElement).toHaveBeenCalledWith('/dashboard');
   });
 });
